0c2c10b111d66f224866462479f5651861decaf0
aliamahgoub94-hub <aliamahgoub94@gmail.com>
2026-04-02 23:10:51 +1300

    Remove middleware

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: [],
+  },
+}
 
 module.exports = nextConfig
+```
+
+**Ctrl+S**, close, then:
+```
+git add next.config.js
+git commit -m "Fix next config"
+git push origin main --force
